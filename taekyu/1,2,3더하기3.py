682cdec6635460e9ae1fa7b7fdf2b64713408a7c
박태규 Park Tae Kyu <ptq124@naver.com>
2024-02-28 23:43:58 +0900

    2/28

---
+++ taekyu/1,2,3더하기3.py
@@ -0,0 +1,15 @@
+import sys
+
+t = int(sys.stdin.readline())
+n = [int(sys.stdin.readline()) for _ in range(t)]
+
+dp = [0] * 1000001
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 1000001):
+    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009
+
+[print(dp[j]) for j in n]
