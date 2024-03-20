4819a391a9ac3355dd6ca51b1e7886988356c767
박태규 Park Tae Kyu <ptq124@naver.com>
2024-03-20 23:36:25 +0900

    3/20

---
+++ taekyu/날짜계산.py
@@ -0,0 +1,24 @@
+e,s,m = map(int, input().split())
+
+a,b,c = 1, 1, 1
+cnt=1
+
+while True:
+  if a==e and b==s and c==m:
+    break
+
+  a+=1
+  b+=1
+  c+=1
+
+  cnt+=1
+
+  if a>15:
+    a = 1
+  if b>28:
+    b = 1
+  if c>19:
+    c = 1
+
+
+print(cnt)
