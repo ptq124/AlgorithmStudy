f34a61c24eb0d7031c62bb75b5be1bc5a6709c64
jiyeon <rlawldus3412@gmail.com>
2024-04-03 23:17:04 +0900

    4/3

--- jiyeon/BOJ. 6064.py
+++ jiyeon/BOJ. 6064.py
@@ -1,2 +1,20 @@
 #카잉 달력
 
+# if x<M : x' =x+1
+# if y<N : y' = y+1 else y'=1
+
+import sys
+input = sys.stdin.readline
+
+tc = int(input())
+for _ in range(tc):
+    m,n,x,y =map(int,input().split())
+    K = x
+
+    while K <= m * n:
+        if (K - x) % m == 0 and (K - y) % n == 0:
+            print(K)
+            break
+        K += m
+    else:
+        print("-1")
